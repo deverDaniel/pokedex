cf43d821014477dd89285561795941acb6e00902
Daniel <dever.daniel.asf@gmail.com>
2023-05-22 16:51:35 -0300

    second

---
+++ script.js
@@ -0,0 +1,5 @@
+let pokemon = 1
+const pokemonName = document.querySelector(".pokemon_name");
+const pokemonNumber = document.querySelector(".pokemon_number");
+const pokemonImage = document.querySelector(".pokemon_image");
+
